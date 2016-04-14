a545839725ccf76d3d5c49da0d899278645d6aa3
brandonpayne <paynebrandon4@gmail.com>
2016-04-14 09:34:13 -0600

    Added files via upload

---
+++ view/about.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Brandon Payne - About</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="index.php">Payne</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="PayneResume.pdf" target="_blank">Resume</a></li>
+						<li><a href="about.php">About</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+
+					<header class="major">
+						<h2>About</h2>
+						<p>&quot;Family is not an important thing. It's everything.&quot;   -Michael J. Fox<br>
+						</p>
+					</header>
+
+					<a href="#" class="image fit"><img src="images/familygame3.jpg" alt="Family Picture" /></a>
+				  <p>There's a select few people that you get to keep around for your whole life, and that is your family.  I love and adore my parents and siblings so much.  I know without them I wouldn't have or be able to do a lot of the things that I do.  They support me in countless numbers of ways and I do my best to do the same for them.  (Going left to right: Brother in law Jake, Mother Andrea, Sister Christina, Father Jeff)  I can't leave out my great brother Eric and his Fiance Marryann.  I can't tell you how grateful I am for them.  </p>
+					<p>There's a few things you need to know about me.  First, I'm hands down one of the biggest Utah Jazz fans you'll ever meet.  My dad and I have been going to every game for close to nine years now.  I love basketball, playing and watching.  I'm also a three time fantasy football champion.  Avid fan of the Minnesota Vikings.  Utah Ute fanatic in every way.  I have a huge passion for sports.</p>
+					<p>I have a huge interest in Web Design and Marketing. I took one basic web class and I've been doing it ever since. I love marketing and advertising.  My dream job would be to work for Apple or Adobe as a Web Designer or Web Master.  I've made pages for actual companies before and I hope to keep going and maybe one day make a living off of this.</p>
+
+
+				</div>
+			</section>
+
+		
+	</body>
+</html>
