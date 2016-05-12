a6c66a55c4832da89bdaa30204603c3793ec8ec0
brandonpayne <paynebrandon4@gmail.com>
2016-05-12 06:30:14 -0600

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Brandon Payne</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+	</head>
+
+        <body>
+            <div style="width:100%; height: 75px; color: #fff; background-color: #3D59AB; margin-top:500px; text-align: center;">
+                <p>Copyright © 2016, All Rights Reserved</p>
+            </div>  
+	</body>
+</html>
